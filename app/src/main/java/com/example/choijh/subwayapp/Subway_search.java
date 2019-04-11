106688929de9e16f3334829cc2ed28369390e677
ChoiJH <jhc0875@naver.com>
2019-04-11 15:44:36 +0900

    조회 페이지(초안)

---
+++ app/src/main/java/com/example/choijh/subwayapp/Subway_search.java
@@ -0,0 +1,179 @@
+package com.example.choijh.subwayapp;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Subway_search extends AppCompatActivity {
+    TextView text;
+
+    String key="7a6c6556566a686338384f56675879";
+    String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subway_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        StrictMode.enableDefaults();
+
+        text= (TextView)findViewById(R.id.result);
+    }
+
+
+
+    String getXmlData(String queryUrl){
+        StringBuffer buffer=new StringBuffer();
+        try {
+            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+
+
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();//태그 이름 얻어오기
+
+                        if(tag.equals("item")) ;// 첫번째 검색결과
+                        else if(tag.equals("STATION_CD")){
+                            buffer.append("전철역 코드 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//addr 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("STATION_NM")){
+                            buffer.append("전철역 명 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("LINE_NUM")){
+                            buffer.append("호선 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//cpId
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("FR_CODE")){
+                            buffer.append("외부코드 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//cpNm
+                            buffer.append("\n");
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName(); //태그 이름 얻어오기
+
+                        if(tag.equals("item"))
+                            buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+
+                        break;
+                }
+
+                eventType= xpp.next();
+
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
+            e.printStackTrace();
+        }
+
+        return buffer.toString();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.subway_search, menu);
+
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        searchView.setQueryHint("역 이름으로 검색");
+
+        // SearchView 검색어 입력/검색 이벤트 처리
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { // query가 입력 단어
+                String location = null;
+                try {
+                    location = URLEncoder.encode(query,"utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                final String queryUrl="http://openapi.seoul.go.kr:8088/"+key
+                        +"/xml/SearchInfoBySubwayNameService/1/5/"+location;
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+
+                        data = getXmlData(queryUrl);//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+                        System.out.println("이건 한글이다.                                     "+data);
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+                    }
+                }).start();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(Subway_search.this, "입력하고있는 단어 = "+newText, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+
+}
