2f82a28a6c608b33c2308e58e8292a1eb4d0e377
hyunhiekim <gus134668@naver.com>
2019-06-12 13:30:56 +0900

    aaa

---
+++ app/src/main/java/com/example/choijh/subwayapp/Bus_favorite.java
@@ -0,0 +1,14 @@
+package com.example.choijh.subwayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bus_favorite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_favorite);
+    }
+}
---
+++ app/src/main/java/com/example/choijh/subwayapp/Subway_favorite.java
@@ -0,0 +1,14 @@
+package com.example.choijh.subwayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subway_favorite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subway_favorite);
+    }
+}
